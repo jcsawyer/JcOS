7714ebb5ed33be3cd995e3176cc5d966fa16a2e0
Joseph Sawyer <joseph@jcsawyer.com>
2024-12-25 18:19:11 +0000

    refactor: add some build optimizations

--- build.zig
+++ build.zig
@@ -14,9 +14,26 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .code_model = .tiny,
+        .linkage = .static,
+        .pic = false,
+        .omit_frame_pointer = false,
     });
     kernel.addIncludePath(b.path("src"));
     kernel.setLinkerScript(b.path("src/linker.ld"));
+
+    // Disable features that are problematic in kernel space.
+    kernel.root_module.red_zone = false;
+    kernel.root_module.stack_check = false;
+    kernel.root_module.stack_protector = false;
+    kernel.want_lto = false;
+    // Delete unused sections to reduce the kernel size.
+    kernel.link_function_sections = true;
+    kernel.link_data_sections = true;
+    kernel.link_gc_sections = true;
+    // Force the page size to 4 KiB to prevent binary bloat.
+    kernel.link_z_max_page_size = 0x1000;
+
     b.installArtifact(kernel);
 
     const bin = b.addObjCopy(kernel.getEmittedBin(), .{ .format = .bin });
