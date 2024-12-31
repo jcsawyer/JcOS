445103063ee688255a18e2dc4519cb30edb74889
Joseph Sawyer <joseph@jcsawyer.com>
2024-12-31 00:35:28 +0000

    refactor: replace hardcoded with const to remove duplicate values and unused const

--- src/_arch/aarch64/cpu/boot.zig
+++ src/_arch/aarch64/cpu/boot.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const kernel_init = @import("../../../main.zig").kernel_init;
 
-const CONST_CORE_ID_MASK: usize = 0b11;
+const CONST_CORE_ID_MASK: []const u8 = "0b11";
 
 pub fn boot_asm() []u8 {
     var buff: [2048]u8 = undefined;
     const asm_code = @embedFile("boot.s");
     @setEvalBranchQuota(10000);
-    _ = std.mem.replace(u8, asm_code, "{CONST_CORE_ID_MASK}", "0b11", buff[0..]);
+    _ = std.mem.replace(u8, asm_code, "{CONST_CORE_ID_MASK}", CONST_CORE_ID_MASK[0..], buff[0..]);
     return buff[0..];
 }
 
